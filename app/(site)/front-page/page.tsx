ee4afe6de8358a4fedb7b6d3c43c47eeaf4f4c6c
= <=>
2023-12-26 06:21:03 +0000

    trying to pull still on my docker

--- app/(site)/front-page/page.tsx
+++ app/(site)/front-page/page.tsx
@@ -6,6 +6,8 @@ export default function Counter() {
   const [count, setCount] = useState(0)
  
   return (
+
+    
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
